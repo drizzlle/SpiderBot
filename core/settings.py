1a6bcff0faace46013f40c2938bdcced1d7b6d3b
Umar Aliyu Chalawa <liuomar90@gmail.com>
2025-12-02 10:50:00 +0100

    #debug: false

--- core/settings.py
+++ core/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [".vercel.app", "127.0.0.1", "localhost"]
 # CSRF_TRUSTED_ORIGINS = ["https://spiderbot-p1x3.onrender.com", "https://127.0.0.1:8000"]
